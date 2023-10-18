be623044175a12200636a3156f9e45e811ed23a6
Simon Oro <simonoro1@outlook.com>
2023-10-18 11:57:29 -0300

    login-tialwind

--- src/App.js
+++ src/App.js
@@ -7,6 +7,10 @@ function App() {
       <div className="w-full flex items-center justify-center lg:w-1/2">
         <SignIn/>
       </div>
+      <div className="hidden relative lg:flex h-full w-1/2 items-center justify-center bg-gray-200">
+        <div className="w-60 h-60 bg-gradient-to-tr from-violet-500 to-pink-500 rounded-full animate-spin"></div>
+        <div className="w-full h-1/2 absolute bottom-0 bg-white/10 backdrop-blur-lg"/>
+      </div>
     </div>
   );
 }
--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,56 +1,123 @@
-import React, { ChangeEvent, useState } from 'react'
-import { useSignIn } from '../hooks/useSignIn';
-import { useNavigate } from 'react-router-dom';
+import React, { ChangeEvent, useState } from "react";
+import { useSignIn } from "../hooks/useSignIn";
+import { useNavigate } from "react-router-dom";
 
 function SignIn() {
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-    const {error, isPending, signin} = useSignIn()
-    const nav = useNavigate()
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { error, isPending, signin } = useSignIn();
+  const nav = useNavigate();
 
-        // Handling the name change
+  // Handling the name change
 
-    // Handling the email change
-    const handleEmail = (e: ChangeEvent<HTMLInputElement>) => {
-        setEmail(e.target.value);
+  // Handling the email change
+  const handleEmail = (e: ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+  };
+
+  // Handling the password change
+  const handlePassword = (e: ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    const user = {
+      name: email,
+      password,
     };
- 
-    // Handling the password change
-    const handlePassword = (e: ChangeEvent<HTMLInputElement>) => {
-        setPassword(e.target.value);
-    };
 
+    console.log(user);
 
-    const handleSubmit = (e: any) => {
-        e.preventDefault()
-        const user = {
-            name: email,
-            password
-        }
+    signin(user);
+    setEmail("");
+    setPassword("");
+    // signin hook
 
-        console.log(user)
-
-        signin(user)
-        setEmail('')
-        setPassword('')
-        // signin hook
-
-        nav('/')
-    }
+    nav("/");
+  };
 
   return (
-    <div className='flex w-full h-screen'>
-        <div className="w-full flex items-center justify-center lg:w-1/2"></div>
-        <h2 className='text-4xl'>SIGN IN</h2>
-        <form  onSubmit={handleSubmit}>
-            <span>E-MAIL</span>
-            <input type="text" value={email} onChange={handleEmail} required/>
-            <span>PASSWORD</span>
-            <input type="password" value={password} onChange={handlePassword} required/>
-            <button>SIGN IN</button>
-        </form>
+    <div className="bg-white px-10 py-20 rounded-3xl border-2 border-gray-100">
+      <h1 className="text-5xl font-semibold">Welcome Back</h1>
+      <p className="font-medium text-lg text-gray-500 mt-4">
+        Welcome back! Please enter your details.
+      </p>
+      <form onSubmit={handleSubmit} className="mt-8">
+        <div className="">
+          <label className="text-lg font-medium">Email</label>
+          <input
+            className="w-full border-2 border-gray-100 rounded-xl p-4 mt-1 bg-transaprent"
+            type="email"
+            placeholder="Enter your email"
+            value={email}
+            onChange={handleEmail}
+            required
+          />
+        </div>
+        <div className="">
+          <label className="text-lg font-medium">Password</label>
+          <input
+            className="w-full border-2 border-gray-100 rounded-xl p-4 mt-1 bg-transaprent"
+            type="password"
+            placeholder="Enter your password"
+            value={password}
+            onChange={handlePassword}
+            required
+          />
+        </div>
+        <div className="mt-8 flex justify-between items-center">
+          <div className="">
+            <input type="checkbox" id="remember" />
+            <label className="ml-2 font-medium text-base" htmlFor="remember">
+              Remember for 30days
+            </label>
+          </div>
+          <button className="font-medium text-base text-violet-500">
+            Forgot password
+          </button>
+        </div>
+        <div className="mt-8 flex flex-col gap-4">
+          <button className="active:scale-[.98] active:duration-75 transition-all hover:scale-[1.01] ease-in-out py-3 rounded-xl bg-violet-500 text-white text-lg font-bold">
+            Sign in
+          </button>
+          <button className="flex rounded-xl py-3  border-2 items-center justify-center gap-2 active:scale-[.98] active:duration-75 transition-all hover:scale-[1.01] ease-in-out">
+            <svg
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M5.26644 9.76453C6.19903 6.93863 8.85469 4.90909 12.0002 4.90909C13.6912 4.90909 15.2184 5.50909 16.4184 6.49091L19.9093 3C17.7821 1.14545 15.0548 0 12.0002 0C7.27031 0 3.19799 2.6983 1.24023 6.65002L5.26644 9.76453Z"
+                fill="#EA4335"
+              />
+              <path
+                d="M16.0406 18.0142C14.9508 18.718 13.5659 19.0926 11.9998 19.0926C8.86633 19.0926 6.21896 17.0785 5.27682 14.2695L1.2373 17.3366C3.19263 21.2953 7.26484 24.0017 11.9998 24.0017C14.9327 24.0017 17.7352 22.959 19.834 21.0012L16.0406 18.0142Z"
+                fill="#34A853"
+              />
+              <path
+                d="M19.8342 20.9978C22.0292 18.9503 23.4545 15.9019 23.4545 11.9982C23.4545 11.2891 23.3455 10.5255 23.1818 9.81641H12V14.4528H18.4364C18.1188 16.0119 17.2663 17.2194 16.0407 18.0108L19.8342 20.9978Z"
+                fill="#4A90E2"
+              />
+              <path
+                d="M5.27698 14.2663C5.03833 13.5547 4.90909 12.7922 4.90909 11.9984C4.90909 11.2167 5.03444 10.4652 5.2662 9.76294L1.23999 6.64844C0.436587 8.25884 0 10.0738 0 11.9984C0 13.918 0.444781 15.7286 1.23746 17.3334L5.27698 14.2663Z"
+                fill="#FBBC05"
+              />
+            </svg>
+            Sign in with Google
+          </button>
+        </div>
+        <div className="mt-8 flex justify-center items-center">
+          <p className="font-medium text-base">Don't have an account?</p>
+          <button className="ml-2 font-medium text-base text-violet-500">
+            Sign up
+          </button>
+        </div>
+      </form>
     </div>
-  )
+  );
 }
 
-export default SignIn
+export default SignIn;
